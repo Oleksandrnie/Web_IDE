26b3771801bc68081426b6710757b7920b300007
Oleksandr Niemchenko <alexandrnemchenko97@gmail.com>
2024-12-21 17:48:15 +0300

    добавил память чтобы было интереснее кликать по не залил другой функционал)

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
+import React from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const ls = localStorage;
+  const [count, setCount] = React.useState(Number(ls.getItem('counter')) || 0);
+
+  const toLS = () => {
+    setCount((prevCount) => {
+      const newCount = prevCount + 1;
+      ls.setItem('counter', JSON.stringify(newCount));
+      return newCount;
+    });
+  };
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank" />
-        <a href="https://react.dev" target="_blank" />
+        <a href="https://vite.dev" target="_blank" rel="noopener noreferrer" />
+        <a href="https://react.dev" target="_blank" rel="noopener noreferrer" />
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={toLS}>
           count is {count}
         </button>
         <p>
@@ -23,7 +32,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
