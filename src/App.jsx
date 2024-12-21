74f246e4647050e64888aded145a94a29aeec443
Oleksandr Niemchenko <alexandrnemchenko97@gmail.com>
2024-12-21 17:54:09 +0300

    дополнение к прошлому коммиту, убрал лишний код оставил только кликер

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,27 @@ import './App.css';
 import React from 'react';
 
 function App() {
-  const ls = localStorage;
-  const [count, setCount] = React.useState(Number(ls.getItem('counter')) || 0);
+	const ls = localStorage;
+	const [count, setCount] = React.useState(Number(ls.getItem('counter')) || 0);
 
-  const toLS = () => {
-    setCount((prevCount) => {
-      const newCount = prevCount + 1;
-      ls.setItem('counter', JSON.stringify(newCount));
-      return newCount;
-    });
-  };
+	const toLS = () => {
+		setCount((prevCount) => {
+			const newCount = prevCount + 1;
+			ls.setItem('counter', JSON.stringify(newCount));
+			return newCount;
+		});
+	};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noopener noreferrer" />
-        <a href="https://react.dev" target="_blank" rel="noopener noreferrer" />
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={toLS}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+	return (
+		<>
+			<h1>Clicker</h1>
+			<div className="card">
+				<button onClick={toLS}>
+					count is {count}
+				</button>
+			</div>
+		</>
+	);
 }
 
 export default App;
